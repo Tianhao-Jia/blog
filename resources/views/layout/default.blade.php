acb9bc087d3fff9657020561d33b9cad6582c8c8
vagrant <vagrant@homestead>
2023-02-24 18:01:57 +0000

    bootstrap

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>@yield('title','blog app')</title>
-</head>
-<body>
-    @yield('content')
-</body>
+<html>
+  <head>
+    <title>@yield('title', 'Weibo App') - Laravel</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">Login</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
+  </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,16 @@
 @extends('layout.default')
 
 @section('content')
-    <h1>Home page</h1>
-@section('title','home page')
-
+  <div class="bg-light p-3 p-sm-5 rounded">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      There is a link for  <a href="https://google.com">Google</a> click here to search。
+    </p>
+    <p>
+      Everything is start here, author by Tianhao Jia.
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">Register Now</a>
+    </p>
+  </div>
 @stop
