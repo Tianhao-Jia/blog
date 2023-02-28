0815898b79bc0d301f73a3e7403e8faf5871ba14
vagrant <vagrant@homestead>
2023-02-28 18:10:35 +0000

    signup page

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -10,7 +10,7 @@
       Everything is start here, author by Tianhao Jia.
     </p>
     <p>
-      <a class="btn btn-lg btn-success" href="#" role="button">Register Now</a>
+      <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">Register Now</a>
     </p>
   </div>
 @stop
