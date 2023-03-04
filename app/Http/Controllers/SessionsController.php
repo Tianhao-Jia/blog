c184572fcec19e697c02e7560dc97ebc5a41d514
vagrant <vagrant@homestead>
2023-03-04 02:34:25 +0000

    remeber me

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,7 +19,7 @@ class SessionsController extends Controller
            'password' => 'required'
        ]);
 
-       if (Auth::attempt($credentials)) {
+       if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', 'Welcome back!');
             return redirect()->route('users.show', [Auth::user()]);
         }else {
@@ -33,5 +33,6 @@ class SessionsController extends Controller
         Auth::logout();
         session()->flash('success', 'Logout Successful');
         return redirect('login');
+
     }
 }
