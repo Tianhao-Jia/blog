bf9e1c699bc7bcb1e58e61e3f39135edd6262909
vagrant <vagrant@homestead>
2023-03-04 23:16:16 +0000

    devide page

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,12 +7,12 @@
   <div class="list-group list-group-flush">
     @foreach ($users as $user)
       <div class="list-group-item">
-        <img class="mr-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width=32>
-        <a href="{{ route('users.show', $user) }}">
-          {{ $user->name }}
-        </a>
+        @include('users._user')
       </div>
     @endforeach
+  </div>
+  <div class="mt-3">
+    {!! $users->render() !!}
   </div>
 </div>
 @stop
